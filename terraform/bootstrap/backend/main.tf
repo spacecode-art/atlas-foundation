bf3a07b7a3abe6c479d3b84c724e3cc984526343
Dennis kaninu <denniskaninu@gmail.com>
2026-08-01 12:28:09 +0300

    feat: add bootstrap backened (s3 state bucket + DynamoDb lock table) for localStack

---
+++ terraform/bootstrap/backend/main.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "atlas-terraform-state"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "atlas-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
